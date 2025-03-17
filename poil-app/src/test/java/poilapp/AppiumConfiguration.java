df8878df80bf61552bee2d61ff395b67d6568196
DESKTOP-J7G3J07\User <farah.george95@gmail.com>
2025-03-17 10:43:40 +0200

    Initial Commit

---
+++ poil-app/src/test/java/poilapp/AppiumConfiguration.java
@@ -0,0 +1,28 @@
+package com.poilapp;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class AppiumConfiguration {
+    private static String applicationPath = "";
+    private static String appiumURL = "http://127.0.0.1/wd.hub";
+    private static String emulatorName = "";
+
+    public static AndroidDriver driver;
+
+    public static void configureAppium() throws MalformedURLException, URISyntaxException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName(emulatorName);
+        options.setApp(applicationPath);
+
+        driver = new AndroidDriver(new URI(appiumURL).toURL(), options);
+    }
+
+    public static void tearDown() {
+        driver.quit();
+    }
+}
